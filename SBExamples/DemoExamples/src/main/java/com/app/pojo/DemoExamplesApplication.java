c66888592a53a29bf3c3c6161ebfdd99f7e13c2b
TL <TL@gmail.com>
2024-08-13 23:45:26 +0530

    Modified in SB Main Method add int x=10000

--- SBExamples/DemoExamples/src/main/java/com/app/pojo/DemoExamplesApplication.java
+++ SBExamples/DemoExamples/src/main/java/com/app/pojo/DemoExamplesApplication.java
@@ -9,6 +9,7 @@ public class DemoExamplesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoExamplesApplication.class, args);
 		System.out.println("hiii");
+                int i=1000;
 	}
 
 }
